95f4fee3d275992b6dce74a0b73f3815610f5cd0
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-19 22:47:55 +0700

    Tugas 12 Js

---
+++ tugas12.js
@@ -0,0 +1,11 @@
+function panggil() {
+  var buah = ['pisang','jeruk','apel','mangga'];
+  console.log(buah);
+  var buah1= buah.pop()
+  console.log(buah);
+
+ var buah2 = buah.shift()
+
+ return buah
+}
+console.log(panggil());
